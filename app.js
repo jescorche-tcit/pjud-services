a5dc1a6f1d3f96032f0caaa89d258275dcbf3cef
José Escorche <jose@tcit.cl>
2019-03-06 11:18:04 -0300

    WIP: Begin scraping of PJUD page to validate lawyers

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ import LawyerServices from './lib/lawyers'
 const app = express();
 
 app.get('/', function (req, res) {
-  LawyerServices.validate('888888888');
-  res.send('Hello World!');
+  LawyerServices.validate('888888888', (data) => {
+    res.send(data);
+  });
 });
 
 app.listen(8081, function () {
